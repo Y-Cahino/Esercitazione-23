4b9206cc243c88816535970ca1de072cd4597212
cahinosilva.20184 <cahinosilva.20184@LABI2-13PC05>
2023-02-03 13:20:11 +0100

    funzione di aggiunta, funzione di cancellamento, funzione bubble sort

--- Corsi_and_Orsi39_1/Program.cs
+++ Corsi_and_Orsi39_1/Program.cs
@@ -11,8 +11,22 @@ namespace Corsi_and_Orsi39_1
         static void Main(string[] args)
         {
             //declare
-            string[] a = new string[10];
+            int dim = 0;
+            string[] a = new string[dim];
             int n=0;
+            string s;
+            //grandezza array
+            Console.WriteLine("Inserire grandezza array");
+            dim=int.Parse(Console.ReadLine());
+            //inserimento animali
+            for (int i=0; i<dim; i++)
+            {
+                Console.Clear();
+                Console.WriteLine("Inserisci un nome di animale");
+                a[i] = Console.ReadLine();
+            }
+            //cancellamento console
+            Console.Clear();
             //menù
             Console.WriteLine("1 - Aggiunta nomi animali");
             Console.WriteLine("2 - Cancella nomi animali");
@@ -24,14 +38,15 @@ namespace Corsi_and_Orsi39_1
             Console.WriteLine("8 - Visualizza nome più lungo e più corto");
             Console.WriteLine("9 - Cancella tutti i nomi uguali");
             int scelta = int.Parse(Console.ReadLine());
+            Console.Clear();
             do
             {
                 switch (scelta)
                 {
                     case 1:
                         Console.WriteLine("Inserire nome:");
-                        a[n] = Console.ReadLine();
-                        if (aggiunta(a, ref n);
+                        s = Console.ReadLine();
+                        if (aggiunta(ref a, s, ref dim))
                         {
                                 Console.WriteLine("Nome inserito correttamente");
                         }
@@ -45,16 +60,80 @@ namespace Corsi_and_Orsi39_1
 
         }
         //funzione di aggiunta
-        static bool aggiunta(string []a,ref int n)
+        static bool aggiunta(ref string []a,string s,ref int i)
         {
-            while (a != null)
+            //variabile per controllare se il vettore è pieno o meno
+            bool inserimento = true;
+            //controllo di riempimento
+            if(i<a.Length)
             {
-                for (n = 0; n < a.Length; n++)
+                while (a != null)
                 {
-                    
+                    a[i] = s;
+                    i++;
+                }
+               
+            }
+            else
+            {
+                inserimento = false;
+            }
+            //ritorna il valore della variabile booleana
+            return inserimento;
+        }
+        //funzione di cancellamento
+        static bool canc(ref string []a,string s, ref int i)
+        {
+            //esisenza stringa
+            bool esiste = false;
+            //ciclo controllo
+            for(int j=0;j<i;j++)
+            {
+                if(a[j]== s)
+                {
+                    i--;
+                    //ciclo di spostamento per la cancellazione
+                    for(int k=j;k<a.Length;k++)
+                    {
+                        a[k] = a[k + 1];
+                    }
+                    esiste = true;
+                    break;
                 }
             }
-            return 0;
+            return esiste;
         }
+        // Funzione per riempire il vettore con numeri casuali
+
+        void riempi(ref string []a, ref int n, int max)
+        {
+            int conta;
+            srand(time(0));
+            for (conta = 0; conta < n; conta++)
+            {
+                a[conta] = ();
+            }
+        }
+
+        // Funzione per ordinare il vettore utilizzando il metodo Shell Sort
+
+        void ShellSort(ref string []a, ref int n)
+        {
+            for (int gap = n / 2; gap > 0; gap /= 2)    // inizializzazione del gap
+            {
+                for (int i = gap; i < n; i++)       // confronto e scambio degli elementi all'interno del sottogruppo
+                {
+                    string temp = a[i];      // salvataggio dell'elemento corrente
+                    int j;
+                    for (j = i; j >= gap && vet[j - gap] > temp; j -= gap
+            
+        }
+            }
+        }
+
+
+
+
+
     }
 }
